c7fef3b053ac20b3cff4970e3fb12ca47ed81547
NguyenThanhAI <37617026+NguyenThanhAI@users.noreply.github.com>
2020-03-18 17:27:17 +0700

    Test okay

--- run_pose_annotator.py
+++ run_pose_annotator.py
@@ -1,6 +1,9 @@
 import os
 import argparse
+import json
 import time
+
+from tqdm import tqdm
 
 import numpy as np
 
@@ -35,7 +38,7 @@ def enumerate_videos(args):
         for file in files:
             if file.endswith((".mp4", ".avi", ".mkv")):
                 videos_list.append(os.path.join(dirs, file))
-
+    #print(videos_list)
     return videos_list
 
 
@@ -58,7 +61,7 @@ def enumerate_videos_folder(args):
             continue
 
         videos_dict.append(dirs)
-
+    #print(videos_dict)
     return videos_dict
 
 
@@ -84,11 +87,11 @@ if __name__ == '__main__':
     if args.run_format == "videos":
         videos_list =  enumerate_videos(args)
 
-        if not os.path.exists("file.txt"):
+        if not os.path.exists("videos_file.txt"):
             start_index = 0
 
         else:
-            with open("file.txt", "r") as f:
+            with open("videos_file.txt", "r") as f:
                 content = f.read()
                 content = content.split("\n")
                 content = list(filter(None, content))
@@ -98,57 +101,73 @@ if __name__ == '__main__':
                 start_index = videos_list.index(content[0])
 
                 f.close()
-
-        for i in range(start_index, len(videos_list)):
+        #print(start_index)
+        for i in tqdm(range(start_index, len(videos_list))):
             video = videos_list[i]
-
+            print("Video ", i)
             json_filename = os.path.basename(video).split(".")[0] + ".json"
-
+            print(json_filename)
             dirname_compose = os.path.dirname(video).split(os.sep)
             relative_dirname = os.path.join(*diff_list(dirname_compose, dataset_dir_compose))
             out_dirname = os.path.join(args.output_dir, relative_dirname)
+            #print(dirname_compose, relative_dirname, out_dirname)
 
             if not os.path.exists(out_dirname):
                 os.makedirs(out_dirname, exist_ok=True)
 
             frames = mmcv.VideoReader(video)
 
+            with open("videos_file.txt", "w") as f:
+                f.write(video + "\n")
+                f.close()
+            video_info = {}
             for j, frame in enumerate(frames):
-
-                with open("file.txt", "w") as f:
-                    f.write(video + "\n")
-                    f.close()
-
+                print("frame ", j, len(frames))
+                height, width = frame.shape[:2]
                 frame_bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
                 body_parts, all_peaks, subset, candidate = extract_parts(frame_bgr, params, model, model_params)
 
                 canvas = draw(frame, all_peaks, subset, candidate)
 
+                frame_info = {}
                 for k, person in enumerate(subset):
+                    frame_info["person_" + str(k).zfill(2)] = {}
                     for h, point in enumerate(person):
                         if h == 18:
                             break
 
                         if point == -1.:
+                            frame_info["person_" + str(k).zfill(2)][label_to_keypoint[h]] = {"x": 0., "y": 0., "prob": 0.}
                             continue
 
                         x = int(candidate[point.astype(int), 0])
                         y = int(candidate[point.astype(int), 1])
                         prob = candidate[point.astype(int), 2]
+                        frame_info["person_" + str(k).zfill(2)][label_to_keypoint[h]] = {"x": round((x + 1) / width, 3), "y": round((y + 1) / height, 3), "prob": round(prob, 3)}
 
-                cv2.imshow("canvas", canvas)
-                cv2.waitKey(2000)
-                cv2.destroyWindow("canvas")
+                video_info["frame_" + str(j).zfill(5)] = frame_info
+                #print(video_info)
+#
+                #with open(os.path.join(out_dirname, json_filename), "w+") as f:
+                #    json.dump(video_info, f, indent=4)
+#
+                #cv2.imshow("canvas", canvas)
+                cv2.imwrite("image_to_vide/image" + str(j).zfill(5) + ".jpg", canvas)
+                #cv2.waitKey(2000)
+                #cv2.destroyWindow("canvas")
+
+            with open(os.path.join(out_dirname, json_filename), "w+") as f:
+                json.dump(video_info, f, indent=4)
 
     else:
         videos_dict = enumerate_videos_folder(args)
 
-        if not os.path.exists("file.txt"):
+        if not os.path.exists("images_file.txt"):
             start_index = 0
 
         else:
-            with open("file.txt", "r") as f:
+            with open("images_file.txt", "r") as f:
                 content = f.read()
                 content = content.split("\n")
                 content = list(filter(None, content))
@@ -158,40 +177,63 @@ if __name__ == '__main__':
                 start_index = videos_dict.index(content[0])
 
                 f.close()
-
-        for i in range(start_index, len(videos_dict)):
+        #print(start_index)
+        for i in tqdm(range(start_index, len(videos_dict))):
             video = videos_dict[i]
 
-            json_filename = video.split(os.sep)[-1].split(".")[0] + ".json"
-
+            json_filename = video.split(os.sep)[-1] + ".json"
+            #(json_filename)
             dirname_compose = video.split(os.sep)[:-1] # [:-1] important
             relative_dirname = os.path.join(*diff_list(dirname_compose, dataset_dir_compose))
             out_dirname = os.path.join(args.output_dir, relative_dirname)
+            #print(dirname_compose, relative_dirname, out_dirname)
 
             if not os.path.exists(out_dirname):
                 os.makedirs(out_dirname, exist_ok=True)
 
+            with open("images_file.txt", "w") as f:
+                f.write(video + "\n")
+                f.close()
+            video_info = {}
             images_list = os.listdir(video)
+            #print(images_list)
 
-            for image in images_list:
+            for j, image in enumerate(images_list):
                 assert image.endswith((".jpg", ".png", ".jpeg"))
+                #print(j, image)
 
                 image_path = os.path.join(video, image)
 
                 img = cv2.imread(image_path)
+                height, width = img.shape[:2]
 
                 img_bgr = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
                 body_parts, all_peaks, subset, candidate = extract_parts(img_bgr, params, model, model_params)
 
+                canvas = draw(img, all_peaks, subset, candidate)
+                frame_info = {}
                 for k, person in enumerate(subset):
+                    frame_info["person_" + str(k).zfill(2)] = {}
                     for h, point in enumerate(person):
                         if h == 18:
                             break
 
                         if point == -1.:
+                            frame_info["person_" + str(k).zfill(2)][label_to_keypoint[h]] = {"x": 0., "y": 0., "prob": 0.}
                             continue
 
                         x = int(candidate[point.astype(int), 0])
                         y = int(candidate[point.astype(int), 1])
                         prob = candidate[point.astype(int), 2]
+                        frame_info["person_" + str(k).zfill(2)][label_to_keypoint[h]] = {"x": round((x + 1) / width, 3), "y": round((y + 1) / height, 3), "prob": round(prob, 3)}
+
+                video_info["frame_" + str(j).zfill(5)] = frame_info
+                #print(video_info)
+#
+                #cv2.imshow("canvas", canvas)
+                #cv2.waitKey(2000)
+                #cv2.destroyWindow("canvas")
+
+            with open(os.path.join(out_dirname, json_filename), "w+") as f:
+                json.dump(video_info, f, indent=4)
