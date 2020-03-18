b60f5fb2f24780bf99e2f5dc68e5103ee13ea75b
NguyenThanhAI <37617026+NguyenThanhAI@users.noreply.github.com>
2020-03-18 17:27:17 +0700

    Test okay

--- demo_image.py
+++ demo_image.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     image_path = args.image
-    output = os.path.basename(image_path).split(".")[0] + "_" + args.output
+    output = os.path.join(os.path.dirname(image_path), os.path.basename(image_path).split(".")[0] + "_" + args.output)
     keras_weights_file = args.model
 
     keypoint_to_label = {'nose': 0, 'neck': 1, 'right_shoulder': 2, 'right_elbow': 3, 'right_wrist': 4, 'left_shoulder': 5, 'left_elbow': 6, 'left_wrist': 7, 'right_hip': 8, 'right_knee': 9, 'right_ankle': 10, 'left_hip': 11, 'left_knee': 12, 'left_ankle': 13, 'right_eye': 14, 'left_eye': 15, 'right_ear': 16, 'left_ear': 17}
@@ -67,6 +67,7 @@ if __name__ == '__main__':
             if i == 18:
                 break
             if point == -1.:
+                results["person_" + str(j)][label_to_keypoint[i]] = {"x": 0., "y": 0.}
                 continue
             x = int(candidate[point.astype(int), 0])
             y = int(candidate[point.astype(int), 1])
@@ -75,11 +76,11 @@ if __name__ == '__main__':
             results["person_" + str(j)][label_to_keypoint[i]] = {"x": round((x + 1) / width, 3), "y": round((y + 1) / height, 3)}
             #cv2.imshow("draw_img", draw_image)
             #cv2.waitKey(0)
-    with open(os.path.basename(image_path).split(".")[0] + ".json", "w+") as f:
+    with open(os.path.join(os.path.dirname(image_path), os.path.basename(image_path).split(".")[0] + ".json"), "w+") as f:
         json.dump(results, fp=f)
 
-    cv2.imshow("", draw_image)
-    cv2.waitKey(0)
+    #cv2.imshow("", draw_image)
+    #cv2.waitKey(0)
 
     cv2.imwrite(output, canvas)
 
